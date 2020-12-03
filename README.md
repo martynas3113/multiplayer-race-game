fec90ee92c85fbcf146c94d9b514c2cf79b5327d
martynas3113 <martynas3113@users.noreply.github.com>
2020-12-03 16:37:41 +0200

    racing

---
+++ README.md
@@ -0,0 +1,11 @@
+# Multiplayer racing game
+
+Description...
+
+## Features
+- playable on single device (computer)
+- single track (straight line)
+- max 2 players
+
+## Link
+You can play this game here: ...
---
+++ js/main.js
@@ -0,0 +1,43 @@
+const car1 = document.querySelector ('.car-1');
+const car2 = document.querySelector ('.car-2');
+
+const models = ['bmw', 'golf' , 'orange']
+const windowWidth = innerWidth;
+const carWidth = 150;
+const finishWidth = 40
+const finishRightSpace = 10
+const carStep = 30;
+
+const finishPosition = windowWidth - carWidth - finishWidth - finishRightSpace;
+const car1Model = Math.floor(Math.random()* models.length);
+const car2Model = Math.floor(Math.random()* models.length);
+
+car1.dataset.model = models[car1Model] 
+car2.dataset.model = models[car2Model] 
+
+let car1Position = 0;
+let car2Position = 0;
+
+car1.style.left = car1Position
+car2.style.left = car2Position
+ 
+ addEventListener('keyup', event=> {
+     const key = event.key;
+
+     if(key === 'w'){
+         car1Position += carStep
+         car1.style.left = car1Position + 'px';
+         if (car1Position > finishPosition){
+             alert('Laimejp pirma masina')
+             location.reload()
+         }
+     }
+     if(key === 'p'){
+        car2Position += carStep
+        car2.style.left = car2Position + 'px';
+        if (car2Position > finishPosition){
+            alert('Laimejp antra masina')
+            location.reload()
+        }
+    }
+ })
